46715ea5d73f91af0b4e47ee99a8ea2af5ff87ca
Irfa Ahmad <irfa.ahmed.31@gmail.com>
2021-05-11 04:23:21 -0400

    get input values

--- reducer.js
+++ reducer.js
@@ -1,1 +1,16 @@
 // file in which the reducing algorithm takes place!
+
+function reduceMatrix() {
+  var matrix = [];
+  for (var i = 0; i < 4; ++i) {
+    for (var j = 0; j < 4; ++j) {
+      var input = "e" + (i+1) + "x" + (j+1);
+      var value = document.getElementById(input);
+      matrix.push(input);
+    }
+  }
+
+
+}
+
+function reduceAlgorithm
