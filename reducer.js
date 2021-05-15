e4046affe8bd04ea5bee0c8b6332a42577092bea
Irfa Ahmad <irfa.ahmed.31@gmail.com>
2021-05-15 01:27:48 -0400

    fixed html bug

--- reducer.js
+++ reducer.js
@@ -3,25 +3,40 @@
 function reduceMatrix() {
   var matrix = [];
   for (var i = 0; i < 4; ++i) {
+    matrix[i] = [];
     for (var j = 0; j < 4; ++j) {
       var input = "e" + (i+1) + "x" + (j+1);
-      var value = document.getElementById(input);
-      matrix.push(input);
+      var value = document.getElementById(input).value;
+      if (value == "") {/*
+        while (matrix.hasChildNodes()) {
+          matrix.removeChild(matrix.lastChild);
+        }*/
+        alert("Please only enter numbers");
+        return;
+      } else {
+        matrix[i].push(value);
+      }
     }
   }
-
+  console.log(matrix);
   printMatrix(matrix);
+  return false;
+}
+
+function gaussJordanReduction(matrix) {
+
 }
 
 function printMatrix(matrix) {
-  var body = document.getElementsByTagName("body");
+  var body = document.getElementById("matrixInputted");
   var table = document.createElement("table");
 
   for (var i = 0; i < 4; ++i) {
     var tr = document.createElement("tr");
     for (var j = 0; j < 4; ++j) {
       var td = document.createElement("td");
-      td.appendChild(document.createTextNode(matrix[i+j]));
+      var cell = document.createTextNode(matrix[i][j]);
+      td.appendChild(cell);
       tr.appendChild(td);
     }
     table.appendChild(tr);
